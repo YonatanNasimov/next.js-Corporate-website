ccc784ac863d1c557b2225e9ca7bbf0ecc1bfe94
yonatan <yonatannasimov@gmail.com>
2023-07-01 15:37:16 +0300

    ok

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,6 +3,9 @@ import React from "react";
 const year = new Date().getFullYear();
 
 const Footer = () => {
+
+  // console.log("Footer") => exmple of SSR, the rendering is loaded in our server, we can see it in the terminal where we run our Next app, when we dont need some interact with the user or anyone who visit our app the HTML renered come from our server to the client side 'ready for use' and google can index the our site in a better way.
+
   return (
     <div>
       <div>©{year} NASIMOV. All rights reserved</div>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,8 +1,7 @@
+"use client"
 import { links } from "@/utils/staticData/links";
 import Link from "next/link";
 import React from "react";
-
-
 
 const Navbar = () => {
   return (
@@ -14,6 +13,13 @@ const Navbar = () => {
             {link.title}
           </Link>
         ))}
+        <button
+          onClick={() => {
+            console.log("Logged Out...") // we will use 'CSR' because here we need some interact we user who use our app/system/website and its a small component, The first rendering is already done (deatails what importent for google scan already done).
+          }}
+        >
+          Log Out
+        </button>
       </div>
     </header>
   );
