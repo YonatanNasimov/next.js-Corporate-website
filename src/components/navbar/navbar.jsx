8aa5ca6425600170fafd973ef10b41acb8cee7b9
yonatan <yonatannasimov@gmail.com>
2023-07-01 13:26:32 +0300

    ok

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+
+const links = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "PortFolio",
+    url: "/portFolio",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 3,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header>
+      <Link href="/">NASIMOV</Link>
+      <div>
+        {links.map(link => {
+            <Link key={link.id} href={link.url}>{link.title}</Link>
+        })}
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
