a476f8627314d2913134f86394f3f0371ffff9d6
yonatan <yonatannasimov@gmail.com>
2023-07-15 16:34:05 +0300

    ok

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,41 @@
 import React from "react";
-import styles from "./paga.module.css"
+import styles from "./paga.module.css";
+import Image from "next/image";
+import Button from "@/components/button/button";
 
 const Contact = () => {
   return (
-    <div className={styles.contact}>
-      Contact
-      <div></div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.image_container}>
+          <Image
+            alt="contact picture"
+            src="/images/contact.png"
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Write your name.."
+          />
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Write your email.."
+          />
+          <textarea
+            cols={30}
+            rows={10}
+            placeholder="Write us a message"
+            className={styles.textArea}
+          ></textarea>
+          <Button url="#" text="send" />
+        </form>
+      </div>
     </div>
   );
 };
