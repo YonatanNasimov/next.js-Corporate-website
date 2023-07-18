e24c4cbad282d27b28a998f7e5a5ffab0e3b4472
yonatan <yonatannasimov@gmail.com>
2023-07-18 20:02:23 +0300

    ok

--- src/app/blog/[id]/page..jsx
+++ src/app/blog/[id]/page..jsx
@@ -4,7 +4,7 @@ import styles from "./blogId.module.css";
 const BlogPost = () => {
   return (
     <div className={styles.container}>
-      <div>BlogPost</div>
+      <div>BgPost</div>
     </div>
   );
 };
