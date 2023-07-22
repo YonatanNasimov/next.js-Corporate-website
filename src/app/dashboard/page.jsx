2c732c6392dc0e9e0d2b9948171524adb6d54c7f
yonatan <yonatannasimov@gmail.com>
2023-07-22 20:01:53 +0300

    ok

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import styles from "./page.module.css"
+"use client"
+import React, { useEffect, useState } from "react";
+import styles from "./page.module.css";
 
 const Dashboard = () => {
-  return (
-    <div className={styles.container}>Dashboard</div>
-  )
-}
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-export default Dashboard
+  const getData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      setError(true);
+    }
+
+    const payload = await res.json();
+
+    setData(payload);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(data);
+
+  return <div className={styles.container}>Dashboard</div>;
+};
+
+export default Dashboard;
