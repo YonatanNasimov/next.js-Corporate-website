d34d20ec517ae343f74e943e2b508b811b234df2
yonatan <yonatannasimov@gmail.com>
2023-07-22 20:28:41 +0300

    ok

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,34 +1,43 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import useSWR from "swr";
 
 const Dashboard = () => {
-  const [data, setData] = useState([]);
-  const [error, setError] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-  const getData = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-      cache: "no-store",
-    });
-
-    if (!res.ok) {
-      setError(true);
-    }
-
-    const payload = await res.json();
-
-    setData(payload);
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
 
   console.log(data);
-
   return <div className={styles.container}>Dashboard</div>;
 };
 
 export default Dashboard;
+
+// const [data, setData] = useState([]);
+//   const [error, setError] = useState(false);
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   const getData = async () => {
+//     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+//       cache: "no-store",
+//     });
+
+//     if (!res.ok) {
+//       setError(true);
+//     }
+
+//     const payload = await res.json();
+
+//     setData(payload);
+//     setIsLoading(false);
+//   };
+
+//   useEffect(() => {
+//     getData();
+//   }, []);
+
+//   console.log(data);
