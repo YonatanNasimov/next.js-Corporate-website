26ec7b8226a270049c4f24b2872ffa806ba96d3e
yonatan <yonatannasimov@gmail.com>
2023-07-22 20:40:00 +0300

    ok

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,116 +2,42 @@ import React from "react";
 import styles from "./category.module.css";
 import Button from "@/components/button/button";
 import Image from "next/image";
+import { items } from "./data.js";
+import { notFound } from "next/navigation";
+
+const getData = (cat) => {
+  const data = items[cat];
+
+  if (data) {
+    return data;
+  }
+
+  return notFound;
+};
 
 const Category = ({ params }) => {
+  const data = getData(params.category);
+
   return (
     <div className={styles.container}>
       <h2 className={styles.cat_title}>{params.category}:</h2>
-        <div className={styles.item} >
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
           <div className={styles.content}>
-            <h3 className={styles.title}>title</h3>
-            <p className={styles.desc}>desc</p>
+            <h3 className={styles.title}>{item.title}</h3>
+            <p className={styles.desc}>{item.desc}</p>
             <Button text="See More" url="#" />
           </div>
           <div className={styles.img_container}>
             <Image
               className={styles.img}
               fill={true}
-              src="/images/websites.jpg"
-              alt=""
+              src={item.image}
+              alt={item.title}
             />
           </div>
         </div>
-        <div className={styles.item} >
-          <div className={styles.content}>
-            <h1 className={styles.title}>title</h1>
-            <p className={styles.desc}>desc</p>
-            <Button text="See More" url="#" />
-          </div>
-          <div className={styles.img_container}>
-            <Image
-              className={styles.img}
-              fill={true}
-              src="/images/websites.jpg"
-              alt=""
-            />
-          </div>
-        </div>
-        <div className={styles.item} >
-          <div className={styles.content}>
-            <h1 className={styles.title}>title</h1>
-            <p className={styles.desc}>desc</p>
-            <Button text="See More" url="#" />
-          </div>
-          <div className={styles.img_container}>
-            <Image
-              className={styles.img}
-              fill={true}
-              src="/images/websites.jpg"
-              alt=""
-            />
-          </div>
-        </div>
-        <div className={styles.item} >
-          <div className={styles.content}>
-            <h1 className={styles.title}>title</h1>
-            <p className={styles.desc}>desc</p>
-            <Button text="See More" url="#" />
-          </div>
-          <div className={styles.img_container}>
-            <Image
-              className={styles.img}
-              fill={true}
-              src="/images/websites.jpg"
-              alt=""
-            />
-          </div>
-        </div>
-        <div className={styles.item} >
-          <div className={styles.content}>
-            <h1 className={styles.title}>title</h1>
-            <p className={styles.desc}>desc</p>
-            <Button text="See More" url="#" />
-          </div>
-          <div className={styles.img_container}>
-            <Image
-              className={styles.img}
-              fill={true}
-              src="/images/websites.jpg"
-              alt=""
-            />
-          </div>
-        </div>
-        <div className={styles.item} >
-          <div className={styles.content}>
-            <h1 className={styles.title}>title</h1>
-            <p className={styles.desc}>desc</p>
-            <Button text="See More" url="#" />
-          </div>
-          <div className={styles.img_container}>
-            <Image
-              className={styles.img}
-              fill={true}
-              src="/images/websites.jpg"
-              alt=""
-            />
-          </div>
-        </div>
-        <div className={styles.item} >
-          <div className={styles.content}>
-            <h1 className={styles.title}>title</h1>
-            <p className={styles.desc}>desc</p>
-            <Button text="See More" url="#" />
-          </div>
-          <div className={styles.img_container}>
-            <Image
-              className={styles.img}
-              fill={true}
-              src="/images/websites.jpg"
-              alt=""
-            />
-          </div>
-        </div>
+      ))}
     </div>
   );
 };
