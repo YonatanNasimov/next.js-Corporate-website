5e103c3e045cdc5f547bea892962d9f99021c6d6
yonatan <yonatannasimov@gmail.com>
2023-07-24 20:47:29 +0300

    ok

---
+++ src/MongoDB/DBConnection.js
@@ -0,0 +1,12 @@
+import config from "@/secret/config";
+
+const connect = async () => {
+  try {
+    await mongoose.connect(`${config.MONGO_DB_URL}`);
+    console.log("mongoDB connected!");
+  } catch (err) {
+    console.log("There is error connecting to mongoDB: " + err);
+  }
+};
+
+export default connect;
--- src/secret/config.js
+++ src/secret/config.js
@@ -1,1 +1,7 @@
 // secret variables
+
+const config = {
+  MONGO_DB_URL: process.env.MONGO_DB_URL
+};
+
+export default config;
