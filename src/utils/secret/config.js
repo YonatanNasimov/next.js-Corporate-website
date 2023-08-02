e42b9e14a1a5e4f41dd71aed4489316b45dcf7fc
yonatan <yonatannasimov@gmail.com>
2023-08-02 20:55:05 +0300

    ok

--- src/utils/secret/config.js
+++ src/utils/secret/config.js
@@ -4,7 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config({});
 
 const config = {
-  MONGO_DB_URL: process.env.MONGO_DB_URL
+  MONGO_DB_URL: process.env.MONGO_DB_URL,
+  API_URL:process.env.API_URL
 };
 
 export default config;
