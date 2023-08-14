85563020f5958bc007f5072ca496f20bd2fee522
yonatan <yonatannasimov@gmail.com>
2023-08-14 21:06:43 +0300

    ok

--- src/app/dashboard/(auth)/signup/page.jsx
+++ src/app/dashboard/(auth)/signup/page.jsx
@@ -1,14 +1,45 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { API_SIGN_UP } from "@utils/apiCalls/urls";
 
 const Signup = () => {
+  const [err, setErr] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const phone = e.target[3].value;
+    const imgUrl = e.target[4].value;
+
+    try {
+      const res = await fetch(API_SIGN_UP, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+          phone,
+          imgUrl,
+        }),
+      });
+    } catch (error) {
+      setErr(true);
+      console.log(error);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Sign Up</h1>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Write your Username"
@@ -41,6 +72,7 @@ const Signup = () => {
         />
         <button className={styles.button}>Sign Up</button>
       </form>
+      {err && "There Was A Problem, Try Again!"}
       <Link className={styles.link} href="/dashboard/login">
         You already have an account? Login
       </Link>
--- src/utils/apiCalls/urls.js
+++ src/utils/apiCalls/urls.js
@@ -3,3 +3,5 @@ import config from "@utils/secret/config";
 const API_URL = config.API_URL;
 
 export const API_POSTS = `${API_URL}/posts`;
+
+export const API_SIGN_UP = `${API_URL}/auth/signup`;
