5d8eab4cfbb2a1385369f5dc10c2a8ff3f8bbea7
yonatan <yonatannasimov@gmail.com>
2023-09-02 19:22:53 +0300

    ok

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,10 @@
 import config from "@utils/secret/config";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connection } from "@utils/mongoDB/DBConnection";
+import userModel from "@utils/mongoDB/models/userModel";
+import bcrypt from "bcryptjs";
 
 const handler = NextAuth({
   providers: [
@@ -8,7 +12,38 @@ const handler = NextAuth({
       clientId: config.GOOGLE_CLIENT_ID,
       clientSecret: config.GOOGLE_CLIENT_SECRET,
     }),
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      async authorize(credentials) {
+        await connection();
+
+        try {
+          const user = userModel.findOne({ email: credentials.email });
+          if (user) {
+            // checked password
+            const isPassword = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+
+            if (isPassword) {
+              return user;
+            } else {
+              throw new Error("Wrong credentials, try again!");
+            }
+          } else {
+            throw new Error("User not found!");
+          }
+        } catch (err) {
+          throw new Error(err);
+        }
+      },
+    }),
   ],
+  pages: {
+    error: "/dashboard/login",
+  },
 });
 
 export { handler as GET, handler as POST };
--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -5,8 +5,31 @@ import styles from "./page.module.css";
 import { signIn } from "next-auth/react";
 
 const Login = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+    console.log("Body_1: " + JSON.stringify({ email, password }));
+    signIn("credentials", { email, password });
+  };
   return (
     <div className={styles.container}>
+      <h1 className={styles.title}>Sign In</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Write your Email"
+          className={styles.input}
+          defaultValue="user1@gmail.com"
+        />
+        <input
+          type="password"
+          placeholder="Write your Password"
+          className={styles.input}
+          defaultValue="user1"
+        />
+        <button className={styles.button}>Sign In</button>
+      </form>
       <button onClick={() => signIn("google")}>Login With Google</button>
     </div>
   );
