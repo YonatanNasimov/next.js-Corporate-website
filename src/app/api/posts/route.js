46f0ef3ec3e61de76fc59acac58f2bc438f62a86
yonatan <yonatannasimov@gmail.com>
2023-09-09 13:05:53 +0300

    ok

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -3,15 +3,15 @@ import { connection } from "@utils/mongoDB/DBConnection";
 import postsModel from "@utils/mongoDB/models/postModel";
 
 export const GET = async (request) => {
-  // const url = new URL(request.url);
+  const url = new URL(request.url);
 
-  // const username = url.searchParams.get("user");
+  const username = url.searchParams.get("username");
 
   try {
     await connection();
 
-    const posts = await postsModel.find(username && { user });
-
+    const posts = await postsModel.find(username && { user: username });
+    
     return new NextResponse(JSON.stringify(posts), {
       status: 200,
     });
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -6,6 +6,7 @@ import useSWR from "swr";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import Spiner from "@components/spiner/spiner";
+import Image from "next/image";
 
 const Dashboard = () => {
   const session = useSession();
@@ -18,7 +19,7 @@ const Dashboard = () => {
     `/api/posts?username=${session?.data?.user.name}`,
     fetcher
   );
-  
+
   console.log(data);
 
   if (session.status === "unauthenticated") {
@@ -29,8 +30,61 @@ const Dashboard = () => {
     return <Spiner />;
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const title = e.target[0].value;
+    const desc = e.target[1].value;
+    const img = e.target[2].value;
+    const content = e.target[3].value;
+
+    try {
+
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   if (session.status === "authenticated") {
-    return <div className={styles.container}>Dashboard</div>;
+    return (
+      <div className={styles.container}>
+        <div className={styles.posts}>
+          {isLoading
+            ? "loading"
+            : data?.map((post) => (
+                <div className={styles.post} key={post._id}>
+                  <div className={styles.img_container}>
+                    <Image
+                      src={post.img}
+                      alt={post.title}
+                      width={200}
+                      height={100}
+                    />
+                  </div>
+                  <h2 className={styles.postTitle}>{post.title}</h2>
+                  <span
+                    className={styles.delete}
+                    onClick={() => handleDelete(post._id)}
+                  >
+                    X
+                  </span>
+                </div>
+              ))}
+        </div>
+        <form className={styles.new} onSubmit={handleSubmit}>
+          <h1>Add New Post</h1>
+          <input type="text" placeholder="Title" className={styles.input} />
+          <input type="text" placeholder="Desc" className={styles.input} />
+          <input type="text" placeholder="Image" className={styles.input} />
+          <textarea
+            placeholder="Content"
+            className={styles.textArea}
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button className={styles.button}>Send</button>
+        </form>
+      </div>
+    );
   }
 };
 
